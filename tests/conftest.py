b4083f78b169b1d0d749f0e65389dd55d9c94b69
denisgaldin <denisgaldin@mail.ru>
2024-11-01 15:29:17 +0300

    New Commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,14 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def set_browser_size():
-    browser.driver.set_window_size(1920, 1080)
-
-
-@pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com/'
-    driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless=new')
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'eager'
+    driver = webdriver.Chrome(options=options)
+    browser.config.driver = driver
 
     yield
 
